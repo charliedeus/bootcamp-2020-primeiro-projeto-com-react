e326e5f696a82845d18d316ba0337dec140c982c
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-06-18 21:38:44 -0300

    Route navigates

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from "react-router-dom";
 import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -18,7 +19,19 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GitHubExplorer:repositories');
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GitHubExplorer:repositories', JSON.stringify(repositories));
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -60,7 +73,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
       {repositories.map(repository => (
-        <a key={repository.full_name} href="Teste">
+        <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
           <img
             src={repository.owner.avatar_url}
             alt={repository.owner.login}
@@ -71,7 +84,7 @@ const Dashboard: React.FC = () => {
           </div>
 
           <FiChevronRight size={20} />
-        </a>
+        </Link>
       ))}
       </Repositories>
     </>
